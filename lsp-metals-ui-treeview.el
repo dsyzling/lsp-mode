fb3237c948f782e0a21b4e9e23ad736e7960df7a
Darren Syzling <darren.syzling@beachhorizon.com>
2019-07-04 16:16:02 +0100

    Initialise treeview state data - store in workspace metadata.
    Use workspace metadata to store metals treeview state data, initially
    buffer of treeview and list of views (view id and view name).

--- lsp-metals-ui-treeview.el
+++ lsp-metals-ui-treeview.el
@@ -22,17 +22,46 @@
 
 ;; lsp-metals treeview ui client - handles a treeview for project tree,
 ;; compilation tree etc.
+;; See the Metals treeview provider spec for further details:
+;;  https://scalameta.org/metals/docs/editors/tree-view-protocol.html
 
 ;;; Code:
 
 (require 'lsp-mode)
 (require 'ht)
 (require 'json)
+(require 'dash)
+
+(cl-defstruct lsp--metals-treeview-data
+  (treeview-buffer nil)
+  (views nil))
 
 (defconst lsp--default-metals-notification-handlers
-  (ht ("metals/treeViewDidChange" #'lsp--metals-treeview-did-change)
-      ))
+  (ht ("metals/treeViewDidChange" #'lsp--metals-treeview-did-change)))
 
+(defun lsp--metals-get-treeview-data (workspace)
+  (lsp-workspace-get-metadata "metals-treeview" workspace))
+
+(defun lsp--metals-set-treeview-data (workspace data)
+  (lsp-workspace-set-metadata "metals-treeview" data workspace))
+
+(defun lsp--metals-init-views (workspace params)
+  (let ((state (make-lsp--metals-treeview-data
+                :treeview-buffer nil
+                :views (mapcar
+                        (lambda (node)
+                          `((:view-id   .  ,(gethash "viewId" node))
+                            (:view-name  .  ,(replace-regexp-in-string "metals" ""
+                                                                      (gethash "viewId" node)))))
+                        (gethash "nodes" params)))))
+    (mapc (lambda (view-data) (lsp-log "found view %s: %s"
+                                       (alist-get :view-id view-data)
+                                       (alist-get :view-name view-data)))
+     (lsp--metals-treeview-data-views state))
+    (lsp--metals-set-treeview-data workspace state)))
+
+(defun lsp--metals-treeview-changed (workspace params treeview-state)
+  (lsp-log "TODO handle treeview changed"))
 
 (defun lsp--metals-treeview-did-change (workspace params)
   "Metals treeview changed notification.
@@ -40,7 +69,43 @@ Nodes that have been changed will be provided within the
 PARAMS message with their viewIds.  WORKSPACE will be the current
 workspace of the project."
   (lsp-log "In lsp--metals-treeview-did-change")
-  (lsp-log (json-encode params)))
+  (lsp-log (json-encode params))
+  ;; check if metals treeview state has been stored or this is the 
+  ;; first time for this project.
+  (let (state (lsp--metals-get-treeview-data workspace))
+    (if (not state)
+        ;; initialise state based on views sent.
+        (lsp--metals-init-views workspace params)
+      (lsp--metals-treeview-changed workspace params state))))
+
+(defun lsp--metals-send-treeview-children(view-id &optional node-uri)
+  "Query children in the view given by VIEW-ID.
+An optional NODE-URI can be used to query children of a specific node
+within the view. This call is asynchronous, the metals method sent
+in this async request is metals/treeViewChildren"
+  (let* ((view-param `(:viewId ,view-id))
+         (params (if node-uri
+                     (append view-param `(:nodeUri ,node-uri))
+                   view-param)))
+    (lsp-request-async "metals/treeViewChildren" params
+                        (lambda (response)
+                          (lsp-log "Return from metals/treeViewChildren")
+                          (lsp-log (json-encode response))))))
+
+
+(defun lsp-metals-treeview-sidebar ()
+  "Display the Metals treeview sidebar window."
+  (interactive)
+  ;; TODO)
+
+;; Testing
+;; (progn
+;;   (switch-to-buffer "build.sbt")
+;;   (lsp--metals-send-treeview-children "metalsBuild"))
+
+;; (progn
+;;   (switch-to-buffer "build.sbt")
+;;   (lsp--metals-send-treeview-children "metalsCompile"))
 
 
 (provide 'lsp-metals-ui-treeview)
